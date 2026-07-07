774a7d729736340272a3034abb0df29bd97b253f
masanarius <masanarius@gmail.com>
2026-07-07 16:44:48 +0900

    Update config.js

--- exp/ist/js/config.js
+++ exp/ist/js/config.js
@@ -135,6 +135,6 @@ const hiddenFeedbackRuleByPlayerMod = { // 得点が「？」になる
     }
 };
 
-const SELECT_COOLDOWN_SEC = 10.0;
-const REFRESH_COOLDOWN_SEC = 5.0;
+const SELECT_COOLDOWN_SEC = 2.0;
+const REFRESH_COOLDOWN_SEC = 2.0;
 const ENABLE_REFRESH_DURING_SELECT_COOLDOWN = false;
